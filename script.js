2599390bb05f9c17059b62e3d464cf4c2bae8129
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-01-17 16:53:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+const todoInput = document.querySelector("#todo-input");
+const addTodoButton = document.querySelector("#add-todo");
+const todoList = document.querySelector("#todo-items");
+
+addTodoButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    const todoValue = todoInput.value;
+    if (!todoValue) {
+        return;
+    }
+    const todo = document.createElement("li");
+    todo.textContent = todoValue;
+    todoList.appendChild(todo);
+    todoInput.value = "";
+});
 
