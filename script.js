d6c61d1dfd5a196a856d2bf4384a07d979243cbe
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-01-17 16:57:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-//your code here
 const todoInput = document.querySelector("#todo-input");
 const addTodoButton = document.querySelector("#add-todo");
 const todoList = document.querySelector("#todo-items");
 
 addTodoButton.addEventListener("click", function(event) {
     event.preventDefault();
-    const todoValue = todoInput.value;
+    const todoValue = todoInput.value.trim();
+
+    //validate the input field
     if (!todoValue) {
+        alert("Please enter a task.");
         return;
     }
+    //create new list item
     const todo = document.createElement("li");
     todo.textContent = todoValue;
+    //append the new list item to the todo list
     todoList.appendChild(todo);
+    //clear the input field
     todoInput.value = "";
 });
-
